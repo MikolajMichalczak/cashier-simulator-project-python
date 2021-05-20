86855795d9eb1c1d854683eafe0d26559dc1c622
MikolajMichalczak <michalczakmikolaj@gmail.com>
2021-05-21 00:49:32 +0200

    generating and showing up items with attributes

--- interface.py
+++ interface.py
@@ -4,9 +4,11 @@ import tkinter.font as fnt
 
 class Ui:
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self, start_callback, item_click_callback):
 
         self.cleared_entry = True
+        self.start_callback = start_callback
+        self.item_click_callback = item_click_callback
 
         self.window = tk.Tk()
         frame_left = tk.Frame(master=self.window, background="red")
@@ -15,46 +17,55 @@ class Ui:
         frame_right = tk.Frame(master=self.window, background="yellow")
         frame_right.pack(side=tk.LEFT, expand=True, fill=tk.BOTH)
 
-        frame_right_numerical_buttons = tk.Frame(master=frame_right, background="yellow")
-        frame_right_numerical_buttons.pack(side=tk.BOTTOM, expand=True, fill=tk.NONE)
+        frame_right_numerical_btns = tk.Frame(master=frame_right, background="yellow")
+        frame_right_numerical_btns.pack(side=tk.BOTTOM, expand=True, fill=tk.NONE)
 
         for i in range(4):
             for j in range(3):
-                numerical_button_frame = tk.Frame(
-                    master=frame_right_numerical_buttons,
+                numerical_btn_frame = tk.Frame(
+                    master=frame_right_numerical_btns,
                     relief=tk.RAISED,
                     borderwidth=1
                 )
-                numerical_button_frame.grid(row=i, column=j, padx=5, pady=5)
+                numerical_btn_frame.grid(row=i, column=j, padx=5, pady=5)
                 if i == 3 and j == 0:
-                    numerical_button = tk.Button(master=numerical_button_frame, text="Wyczyść", padx=15, pady=10,
-                                                 font=fnt.Font(size=10), command=lambda: self.clear_entry())
+                    numerical_btn = tk.Button(master=numerical_btn_frame, text="Wyczyść", padx=15, pady=10,
+                                              font=fnt.Font(size=10), command=lambda: self.clear_entry())
                 elif i == 3 and j == 2:
-                    numerical_button = tk.Button(master=numerical_button_frame, text="Backspace", padx=15, pady=10,
-                                                 font=fnt.Font(size=10), command=lambda: self.backspace_entry())
+                    numerical_btn = tk.Button(master=numerical_btn_frame, text="Backspace", padx=15, pady=10,
+                                              font=fnt.Font(size=10), command=lambda: self.backspace_entry())
                 elif i == 3 and j == 1:
-                    numerical_button = tk.Button(master=numerical_button_frame, text="0", padx=15, pady=10,
-                                                 font=fnt.Font(size=10), command=lambda: self.add_number_to_entry("0"))
+                    numerical_btn = tk.Button(master=numerical_btn_frame, text="0", padx=15, pady=10,
+                                              font=fnt.Font(size=10), command=lambda: self.add_number_to_entry("0"))
                 else:
-                    numerical_button = tk.Button(master=numerical_button_frame, text=str(j + 1 + 3 * i), padx=15,
-                                                 pady=10,
-                                                 font=fnt.Font(size=10),
-                                                 command=(lambda x=(j + 1 + 3 * i): self.add_number_to_entry(str(x))))
-                numerical_button.pack()
+                    numerical_btn = tk.Button(master=numerical_btn_frame, text=str(j + 1 + 3 * i), padx=15,
+                                              pady=10,
+                                              font=fnt.Font(size=10),
+                                              command=(lambda x=(j + 1 + 3 * i): self.add_number_to_entry(str(x))))
+                numerical_btn.pack()
 
-        numerical_button_frame = tk.Frame(
-            master=frame_right_numerical_buttons,
+        numerical_btn_frame = tk.Frame(
+            master=frame_right_numerical_btns,
             relief=tk.RAISED,
             borderwidth=1
         )
-        numerical_button_frame.grid(row=0, column=3, padx=5, pady=5)
-        weigh_button = tk.Button(master=numerical_button_frame, text="Zważ", padx=15, pady=10, font=fnt.Font(size=10))
-        weigh_button.pack()
+        numerical_btn_frame.grid(row=0, column=3, padx=5, pady=5)
+        weigh_btn = tk.Button(master=numerical_btn_frame, text="Zważ", padx=15, pady=10, font=fnt.Font(size=10))
+        weigh_btn.pack()
 
         self.ent_weigh = tk.Entry(master=frame_right, font=fnt.Font(size=20), text="1")
         self.ent_weigh.place(width=300, height=50, relx=0.5, rely=0.15, anchor=tk.CENTER)
         self.ent_weigh.insert(0, 1)
         self.ent_weigh.configure(state=tk.DISABLED)
+
+        self.next_client_btn = tk.Button(master=frame_left, text="Następny klient", padx=15, pady=10,
+                                         font=fnt.Font(size=10),
+                                         command=lambda: self.hide_next_client_btn_and_start())
+        self.next_client_btn.pack(side=tk.BOTTOM, expand=True, fill=tk.NONE)
+
+        self.item_btn = tk.Button(master=frame_left, padx=15, pady=10,
+                                  font=fnt.Font(size=10),
+                                  command=lambda: self.item_click_callback())
 
     def add_number_to_entry(self, number):
         self.ent_weigh.configure(state=tk.NORMAL)
@@ -82,7 +93,17 @@ class Ui:
             self.ent_weigh.delete(0, tk.END)
             self.ent_weigh.insert(0, 1)
         else:
-            self.ent_weigh.delete(len(self.ent_weigh.get())-1, tk.END)
+            self.ent_weigh.delete(len(self.ent_weigh.get()) - 1, tk.END)
             self.cleared_entry = True
 
         self.ent_weigh.configure(state=tk.DISABLED)
+
+    def hide_next_client_btn_and_start(self):
+        self.next_client_btn.pack_forget()
+        self.start_callback()
+
+    def show_item(self, text):
+        self.item_btn['text'] = text
+        self.item_btn.pack(side=tk.BOTTOM, expand=True, fill=tk.NONE)
+
+
--- main.py
+++ main.py
@@ -1,14 +1,94 @@
 import interface
+from datetime import datetime
+import random
+
+item_names = ["Marchewka", "Arbuz", "Chleb", "Makaron", "Dorsz", "Ananas", "Banan", "Ziemniak", "Cukinia", "Pomidor",
+              "Jabłko", "Cytryna", "Czekolada",
+              "Mango", "Gruszka", "Kapusta", "Kiwi", "Groch", "Granat", "Tuńczyk"]
+items_list = []
+items_counter = 0
+start_time = datetime.now()
+items_size = 0
+global current_item_index
+global ui
+global current_item_type
+global current_item
+
+
+class Towar:
+
+    def __init__(self):
+        self.append_time = datetime.now()
+        self.validate_time = datetime.now()
+        self.name = random.choice(item_names)
+
+
+class TowarNaSztuki(Towar):
+
+    def __init__(self):
+        super().__init__()
+        self.quantity = self.getQuantity()
+
+    @staticmethod
+    def getQuantity():
+        is_more_than_one = random.choice([True, False])
+        return random.randint(2, 50) if is_more_than_one else 1
+
+
+class TowarNaWage(Towar):
+
+    def __init__(self):
+        super().__init__()
+        self.weight = round(random.uniform(0.05, 2), 2)
+
+
+def start():
+    global items_counter, start_time, items_size, items_list, current_item_index
+
+    items_counter = 0
+    start_time = datetime.now()
+    items_size = random.randint(10, 20)
+    items_list = shuffle_list_and_return([TowarNaSztuki() if x < int(items_size / 2)
+                                          else TowarNaWage() for x in range(items_size)])
+    current_item_index = 0
+    setCurrentItemInfo(current_item_index)
+    showTowarNaWageItem(current_item) if type(current_item) == TowarNaWage else showTowarNaSztukiItem(current_item)
 
 
 def main():
-    ui = interface.Ui()
+    global ui
+
+    ui = interface.Ui(start, on_item_click)
     window = ui.window
     window.geometry('1000x500')
     window.resizable(False, False)
     window.title("Symulator kasjera")
     window.mainloop()
 
+
+def shuffle_list_and_return(x):
+    random.shuffle(x)
+    return x
+
+
+def setCurrentItemInfo(index):
+    global current_item_type, current_item
+
+    current_item = items_list[index]
+    current_item.append_time = datetime.now()
+    current_item_type = type(current_item)
+
+
+def showTowarNaWageItem(item: TowarNaWage):
+    ui.show_item(item.name+"x"+str(item.weight))
+
+
+def showTowarNaSztukiItem(item: TowarNaSztuki):
+    ui.show_item(item.name+"x"+str(item.quantity))
+
+
+def on_item_click():
+    pass
 
 
 main()
